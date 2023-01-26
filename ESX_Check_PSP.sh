22b90024e271c7a34f5e2a3e9256177f166ce75f
gaetanmarais <79994244+gaetanmarais@users.noreply.github.com>
2023-01-26 18:22:59 +0100

    Create ESX_Check_PSP.sh

---
+++ ESX_Check_PSP.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+#
+#
+# Author      : Gaetan MARAIS
+# Version     : 1.0
+# Date        : 26/01/2023
+# Description : Check path policy for DataCore Virtual disks devices and change it to RoundRobin if needed
+#
+#
+##########################################################
+
+
+#list all DataCore Virtual disks devices (naa.60030d9)
+
+esxcli storage core adapter device list | awk '/naa.60030d9/ {print $2}'| while read NAA
+do
+
+        #dump some data
+        DEVICE=$(esxcli storage nmp device list --device $NAA)
+        NAME=$(echo "$DEVICE"| awk -F":" '/Device Display Name:/ {print $2}')
+        PSP=$(echo "$DEVICE"| awk -F":" '/Path Selection Policy:/ {print $2}')
+
+        NEWPSP=""
+        #check if the PSP is VMware RoundRobin PSP
+        if [[ "$PSP" != " VMW_PSP_RR" ]]
+        then
+
+                #If it's not PSP RR, it will change it to RoundRobin policy
+                esxcli storage nmp device set --device $NAA --psp VMW_PSP_RR
+                echo "$NAA ($NAME) ---> PSP:$PSP changed to VMW_PSP_RR"
+        else
+                echo "$NAA ($NAME) ---> PSP:$PSP"
+        fi
+done
