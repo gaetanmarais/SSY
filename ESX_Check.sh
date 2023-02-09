e9b38d985c35fff5998235de7b046e7d389b5986
gaetanmarais <79994244+gaetanmarais@users.noreply.github.com>
2023-02-09 18:37:30 +0100

    Update ESX_Check.sh

--- ESX_Check.sh
+++ ESX_Check.sh
@@ -23,7 +23,7 @@ DID=$(echo $VALUE|awk '{print $1}'|awk -F":" '{print $2}')
 SVID=$(echo $VALUE|awk '{print $2}'|awk -F":" '{print $1}')
 SSID=$(echo $VALUE|awk '{print $2}'|awk -F":" '{print $2}')
 echo "$VID:$DID $SVID:$SSID"
-echo "https://www.vmware.com/resources/compatibility/search.php?deviceCategory=io&details=1&VID=$VID&DID=$DID&SVID=$VID&SSID=$SSID&page=1&display_interval=10&sortColumn=Partner&sortOrder=Asc"
+echo "https://www.vmware.com/resources/compatibility/search.php?deviceCategory=io&details=1&VID=$VID&DID=$DID&SVID=$SVID&SSID=$SSID&page=1&display_interval=10&sortColumn=Partner&sortOrder=Asc"
 echo "-------------------------------------------------"
 done
 
